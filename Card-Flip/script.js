4a2a785e479bcc6dc8d074d3f77df27e90e8d977
Ayush Chauhan <65812075+AyushChauhan3757@users.noreply.github.com>
2023-09-05 00:39:39 +0530

    Update script.js

--- Card-Flip/script.js
+++ Card-Flip/script.js
@@ -1,76 +1,69 @@
-// Selecting elements from the HTML document
-const cards = document.querySelectorAll(".card"); // Select all elements with the class "card"
-const timeTag = document.querySelector(".time b"); // Select the time display element
-const flipsTag = document.querySelector(".flips b"); // Select the flips display element
-const refreshBtn = document.querySelector(".details button"); // Select the refresh button
+const cards = document.querySelectorAll(".card"),
+    timeTag = document.querySelector(".time b"),
+    flipsTag = document.querySelector(".flips b"),
+    refreshBtn = document.querySelector(".details button");
 
-// Initializing game variables
-let maxTime = 20; // Maximum game time in seconds
-let timeLeft = maxTime; // Current time left in the game
-let flips = 0; // Number of flips made by the player
-let matchedCards = 0; // Number of matched pairs
-let disabledDeck = false; // Flag to prevent card flips while processing
-let isPlaying = false; // Flag to track if the game is in progress
-let cardOne, cardTwo, timer; // Variables to track flipped cards and the timer
+let maxTime = 20;
+let timeLeft = maxTime;
+let flips = 0;
+let matchedCards = 0;
+let disabledDeck = false;
+let isPlaying = false;
+let cardOne, cardTwo, timer;
 
-// Function to initialize and update the game timer
 function initTimer() {
-    if (timeLeft <= 0) {
-        return clearInterval(timer); // Stop the timer when time runs out
+    if(timeLeft <= 0) {
+        return clearInterval(timer);
     }
     timeLeft--;
     timeTag.innerHTML = timeLeft;
 }
 
-// Function to handle card flipping
-function flipCard({ target: clickedCard }) {
-    if (!isPlaying) {
+function flipCard({target: clickedCard}) {
+    if(!isPlaying) {
         isPlaying = true;
-        timer = setInterval(initTimer, 1000); // Start the timer when the first card is flipped
+        timer = setInterval(initTimer, 1000);
     }
-    if (clickedCard !== cardOne && !disabledDeck && timeLeft > 0) {
+    if(clickedCard !== cardOne && !disabledDeck && timeLeft >0) {
         flips++;
         flipsTag.innerText = flips;
         clickedCard.classList.add("flip");
-        if (!cardOne) {
-            cardOne = clickedCard; // Store the first flipped card
-        } else {
-            cardTwo = clickedCard;
-            disabledDeck = true;
-            let cardOneIcon = cardOne.querySelector(".back-view i").classList.value;
-            let cardTwoIcon = cardTwo.querySelector(".back-view i").classList.value;
-            matchCards(cardOneIcon, cardTwoIcon);
+        if(!cardOne) {
+            return cardOne = clickedCard;
         }
+        cardTwo = clickedCard;
+        disabledDeck = true;
+        let cardOneIcon = cardOne.querySelector(".back-view i").classList.value;
+        let cardTwoIcon = cardTwo.querySelector(".back-view i").classList.value;
+        matchCards(cardOneIcon, cardTwoIcon);
     }
 }
 
-// Function to check if two flipped cards match
 function matchCards(icon1, icon2) {
-    if (icon1 === icon2) {
+    if(icon1 === icon2) {
         matchedCards++;
-        if (matchedCards == 6 && timeLeft > 0) {
-            return clearInterval(timer); // Stop the timer when all pairs are matched
+        if(matchedCards == 6 && timeLeft > 0) {
+            return clearInterval(timer);
         }
         cardOne.removeEventListener("click", flipCard);
         cardTwo.removeEventListener("click", flipCard);
         cardOne = cardTwo = "";
-        disabledDeck = false; // Allow further card flips
-    } else {
-        setTimeout(() => {
-            cardOne.classList.add("shake");
-            cardTwo.classList.add("shake");
-        }, 400);
-
-        setTimeout(() => {
-            cardOne.classList.remove("shake", "flip");
-            cardTwo.classList.remove("shake", "flip");
-            cardOne = cardTwo = "";
-            disabledDeck = false;
-        }, 1200);
+        return disabledDeck = false;
     }
+
+    setTimeout(() => {
+        cardOne.classList.add("shake");
+        cardTwo.classList.add("shake");
+    }, 400);
+
+    setTimeout(() => {
+        cardOne.classList.remove("shake", "flip");
+        cardTwo.classList.remove("shake", "flip");
+        cardOne = cardTwo = "";
+        disabledDeck = false;
+    }, 1200);
 }
 
-// Function to shuffle and reset the cards
 function shuffleCards() {
     timeLeft = maxTime;
     flips = matchedCards = 0;
@@ -80,19 +73,12 @@ function shuffleCards() {
     flipsTag.innerText = flips;
     disabledDeck = isPlaying = false;
 
-    let arr = [
-        "bxl-tiktok",
-        "bxl-instagram-alt",
-        "bxl-facebook-circle",
-        "bxl-twitter",
-        "bxl-whatsapp",
-        "bxl-youtube",
-    ];
-    arr.sort(() => (Math.random() > 0.5 ? 1 : -1)); // Shuffle the icon array randomly
+    let arr = ["bxl-tiktok", "bxl-instagram-alt", "bxl-facebook-circle", "bxl-twitter", "bxl-whatsapp", "bxl-youtube"];
+    arr.sort(() => Math.random() > 0.5 ? 1 : -1);
 
     cards.forEach((card, index) => {
         card.classList.remove("flip");
-        let iconTag = card.querySelector(".back-view i");
+        leticonTag = card.querySelector(".back-view i");
         setTimeout(() => {
             iconTag.classList.value = `bx ${arr[index]}`;
         }, 500);
@@ -100,11 +86,10 @@ function shuffleCards() {
     });
 }
 
-// Initial shuffle and setup of event listeners
-shuffleCards(); // Shuffle the cards at the beginning of the game
+shuffleCards();
 
-refreshBtn.addEventListener("click", shuffleCards); // Add event listener to the refresh button
+refreshBtn.addEventListener("click", shuffleCards);
 
-cards.forEach((card) => {
-    card.addEventListener("click", flipCard); // Add event listener to each card for flipping
+cards.forEach((card, index) => {
+    card.addEventListener("click", flipCard);
 });
