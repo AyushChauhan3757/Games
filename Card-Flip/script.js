d65756d8000d727f009300a2d3bd2c075ca81a80
Ayush Chauhan <65812075+AyushChauhan3757@users.noreply.github.com>
2023-09-05 08:18:04 +0530

    Removed shuffling bug

--- Card-Flip/script.js
+++ Card-Flip/script.js
@@ -1,8 +1,10 @@
+// Select all card elements, time element, flips element, and refresh button.
 const cards = document.querySelectorAll(".card"),
     timeTag = document.querySelector(".time b"),
     flipsTag = document.querySelector(".flips b"),
     refreshBtn = document.querySelector(".details button");
 
+// Initialize game variables
 let maxTime = 20;
 let timeLeft = maxTime;
 let flips = 0;
@@ -11,24 +13,26 @@ let disabledDeck = false;
 let isPlaying = false;
 let cardOne, cardTwo, timer;
 
+// Timer function to update the time left
 function initTimer() {
-    if(timeLeft <= 0) {
+    if (timeLeft <= 0) {
         return clearInterval(timer);
     }
     timeLeft--;
     timeTag.innerHTML = timeLeft;
 }
 
-function flipCard({target: clickedCard}) {
-    if(!isPlaying) {
+// Function to handle card flipping
+function flipCard({ target: clickedCard }) {
+    if (!isPlaying) {
         isPlaying = true;
         timer = setInterval(initTimer, 1000);
     }
-    if(clickedCard !== cardOne && !disabledDeck && timeLeft >0) {
+    if (clickedCard !== cardOne && !disabledDeck && timeLeft > 0) {
         flips++;
         flipsTag.innerText = flips;
         clickedCard.classList.add("flip");
-        if(!cardOne) {
+        if (!cardOne) {
             return cardOne = clickedCard;
         }
         cardTwo = clickedCard;
@@ -39,10 +43,11 @@ function flipCard({target: clickedCard}) {
     }
 }
 
+// Function to check if two flipped cards match
 function matchCards(icon1, icon2) {
-    if(icon1 === icon2) {
+    if (icon1 === icon2) {
         matchedCards++;
-        if(matchedCards == 6 && timeLeft > 0) {
+        if (matchedCards == 6 && timeLeft > 0) {
             return clearInterval(timer);
         }
         cardOne.removeEventListener("click", flipCard);
@@ -64,6 +69,7 @@ function matchCards(icon1, icon2) {
     }, 1200);
 }
 
+// Function to shuffle cards and start a new game
 function shuffleCards() {
     timeLeft = maxTime;
     flips = matchedCards = 0;
@@ -73,23 +79,29 @@ function shuffleCards() {
     flipsTag.innerText = flips;
     disabledDeck = isPlaying = false;
 
+    // Array of card icons to shuffle and duplicate for pairs
     let arr = ["bxl-tiktok", "bxl-instagram-alt", "bxl-facebook-circle", "bxl-twitter", "bxl-whatsapp", "bxl-youtube"];
-    arr.sort(() => Math.random() > 0.5 ? 1 : -1);
+    arr = arr.concat(arr); // Duplicate the icons so there are pairs
+    arr.sort(() => Math.random() > 0.5 ? 1 : -1); // Shuffle the icons randomly
 
+    // Assign shuffled icons to cards and add click event listeners
     cards.forEach((card, index) => {
         card.classList.remove("flip");
-        leticonTag = card.querySelector(".back-view i");
+        let iconTag = card.querySelector(".back-view i");
         setTimeout(() => {
-            iconTag.classList.value = `bx ${arr[index]}`;
+            iconTag.className = `bx ${arr[index]}`;
         }, 500);
         card.addEventListener("click", flipCard);
     });
 }
 
+// Initial shuffle to start the game
 shuffleCards();
 
+// Event listener for the refresh button
 refreshBtn.addEventListener("click", shuffleCards);
 
+// Event listener for clicking on cards
 cards.forEach((card, index) => {
     card.addEventListener("click", flipCard);
 });
